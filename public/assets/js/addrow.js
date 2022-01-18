b021eb0cabb942c1c772148be47aafc862a8d671
Gia Tuấn <71738788+anhtuan98h@users.noreply.github.com>
2022-01-18 18:23:04 +0700

    Update addrow.js
    Update Js

--- public/assets/js/addrow.js
+++ public/assets/js/addrow.js
@@ -5,7 +5,7 @@ $(".addRow").on("click", function () {
 function addRow() {
     var tr =
         "<tr>" +
-        '<td><input type="text" name="subject_name[]" class="form-control" required=""></td>' +
+        '<td><select name="subject_id" id="" class="form-control">   <option value="">-----------Chọn-------------</option><option value="3">Tiếng anh chuyên ngành</option> <option value="4">Tin học đại cương</option><option value="7">Lập trình phân tán</option> <option value="15">Cấu trúc dữ liệu và giải thuật</option> <option value="19">Lập trình nâng cao</option> <option value="21">Lập trình java</option> <option value="30">Mạng máy tính</option></select></td>' +
         '<td><input type="text" name="mark[]" class="form-control"></td>' +
         '<td><a href="#" class="btn btn-danger remove"><i class="glyphicon glyphicon-remove"></i></a></td>' +
         "</tr>";
